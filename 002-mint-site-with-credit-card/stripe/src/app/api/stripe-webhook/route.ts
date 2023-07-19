cb5725f1fab1972f0c01c05bc4683556102211d6
ksuhara <ponta.blockchain@gmail.com>
2023-07-19 23:18:26 +0900

    test

--- 002-mint-site-with-credit-card/stripe/src/app/api/stripe-webhook/route.ts
+++ 002-mint-site-with-credit-card/stripe/src/app/api/stripe-webhook/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
   apiVersion: "2022-11-15",
@@ -22,6 +24,7 @@ export async function POST(request: Request) {
   const sig = request.headers.get("stripe-signature") as string;
   let event;
   if (!request.body) return;
+  const body = await request.json();
   const rawBody = await readStream(request.body);
 
   try {
@@ -32,6 +35,29 @@ export async function POST(request: Request) {
     );
   } catch (err: any) {
     return NextResponse.error();
+  }
+
+  switch (event.type) {
+    case "checkout.session.completed":
+      const checkoutSessionCompleted = event.data.object;
+      // Then define and call a function to handle the event checkout.session.completed
+      const metadata = body.data.object.metadata;
+      console.log(metadata);
+      const receiverAddress = metadata.address;
+
+      const sdk = new ThirdwebSDK("mumbai", {
+        secretKey: process.env.SECRET_KEY,
+      });
+
+      const contract = await sdk.getContract(
+        "0xE71eE93Ad57c8b355e1bCDFe435B05673d8B4930"
+      );
+      const tx = await contract.erc721.claimTo(receiverAddress, 1);
+
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
   }
 
   return NextResponse.json({});
