a7d5af25563494520c0e0ac85ddcc3609d57a8ea
ksuhara <ponta.blockchain@gmail.com>
2023-07-19 23:35:16 +0900

    test

--- 002-mint-site-with-credit-card/stripe/src/app/api/stripe-webhook/route.ts
+++ 002-mint-site-with-credit-card/stripe/src/app/api/stripe-webhook/route.ts
@@ -1,3 +1,4 @@
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
@@ -5,32 +6,18 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
   apiVersion: "2022-11-15",
 });
-async function readStream(stream: ReadableStream<Uint8Array>) {
-  const reader = stream.getReader();
-  let result = "";
-
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) {
-      break;
-    }
-    result += new TextDecoder().decode(value);
-  }
-
-  return result;
-}
 
 export async function POST(request: Request) {
-  const sig = request.headers.get("stripe-signature") as string;
-  let event;
-  if (!request.body) return;
+  const bodyString = await request.text();
   const body = await request.json();
-  const rawBody = await readStream(request.body);
+  console.log(body, "body");
+  const signature = headers().get("Stripe-Signature") as string;
+  let event: Stripe.Event;
 
   try {
     event = stripe.webhooks.constructEvent(
-      rawBody,
-      sig,
+      bodyString,
+      signature,
       process.env.STRIPE_ENDPOINT_SECRET || ""
     );
   } catch (err: any) {
@@ -41,9 +28,6 @@ export async function POST(request: Request) {
     case "checkout.session.completed":
       const checkoutSessionCompleted = event.data.object;
       // Then define and call a function to handle the event checkout.session.completed
-      const metadata = body.data.object.metadata;
-      console.log(metadata);
-      const receiverAddress = metadata.address;
 
       const sdk = new ThirdwebSDK("mumbai", {
         secretKey: process.env.SECRET_KEY,
@@ -52,8 +36,9 @@ export async function POST(request: Request) {
       const contract = await sdk.getContract(
         "0xE71eE93Ad57c8b355e1bCDFe435B05673d8B4930"
       );
-      const tx = await contract.erc721.claimTo(receiverAddress, 1);
 
+      const tx = await contract.erc721.claimTo(body.object.metadata.address, 1);
+      console.log(tx);
       break;
     // ... handle other event types
     default:
