a84a1c6335bb9da1c539a55bed75706e01a0f850
ksuhara <ponta.blockchain@gmail.com>
2023-07-20 21:06:34 +0900

    remove

--- 000-ERC721/contracts/SimpleERC721.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-// Uncomment this line to use console.log
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract SimpleERC721 is ERC721 {
-    constructor() ERC721("SimpleERC721", "SNFT") {}
-}
