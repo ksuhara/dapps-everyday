25b77f0c3c26981176e1fcc8886a688f8ace5564
ksuhara <ponta.blockchain@gmail.com>
2023-08-10 00:22:29 +0900

    nft staking

---
+++ 012-nft-staking/thirdweb/app/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+
+import {
+  ConnectWallet,
+  ThirdwebNftMedia,
+  useAddress,
+  useContract,
+  useContractRead,
+  useOwnedNFTs,
+  useTokenBalance,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import { BigNumber, ethers } from "ethers";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
+
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  const nftDropContractAddress = "0xE71eE93Ad57c8b355e1bCDFe435B05673d8B4930";
+  const stakingContractAddress = "0xAad1428cC2410e2C59F55a56618675A98AdA6ee3";
+  const tokenContractAddress = "0x15A142002C032CA499150359Ce2A3ef66c8533B0";
+  const address = useAddress();
+  const { contract: nftDropContract } = useContract(
+    nftDropContractAddress,
+    "nft-drop"
+  );
+  const { contract: tokenContract } = useContract(
+    tokenContractAddress,
+    "token"
+  );
+  const { contract, isLoading } = useContract(stakingContractAddress);
+  const { data: ownedNfts } = useOwnedNFTs(nftDropContract, address);
+  const { data: tokenBalance } = useTokenBalance(tokenContract, address);
+  const [claimableRewards, setClaimableRewards] = useState<BigNumber>();
+  const { data: stakedTokens } = useContractRead(contract, "getStakeInfo", [
+    address,
+  ]);
+
+  useEffect(() => {
+    if (!contract || !address) return;
+
+    async function loadClaimableRewards() {
+      const stakeInfo = await contract?.call("getStakeInfo", [address]);
+      setClaimableRewards(stakeInfo[1]);
+    }
+
+    loadClaimableRewards();
+  }, [address, contract]);
+
+  async function stakeNft(id: string) {
+    if (!address) return;
+
+    const isApproved = await nftDropContract?.isApproved(
+      address,
+      stakingContractAddress
+    );
+    if (!isApproved) {
+      await nftDropContract?.setApprovalForAll(stakingContractAddress, true);
+    }
+    await contract?.call("stake", [[id]]);
+  }
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.h1}>Stake Your NFTs</h1>
+      <hr className={`${styles.divider} ${styles.spacerTop}`} />
+
+      {!address ? (
+        <ConnectWallet />
+      ) : (
+        <>
+          <h2>Your Tokens</h2>
+          <div className={styles.tokenGrid}>
+            <div className={styles.tokenItem}>
+              <h3 className={styles.tokenLabel}>Claimable Rewards</h3>
+              <p className={styles.tokenValue}>
+                <b>
+                  {!claimableRewards
+                    ? "Loading..."
+                    : ethers.utils.formatUnits(claimableRewards, 18)}
+                </b>{" "}
+                {tokenBalance?.symbol}
+              </p>
+            </div>
+            <div className={styles.tokenItem}>
+              <h3 className={styles.tokenLabel}>Current Balance</h3>
+              <p className={styles.tokenValue}>
+                <b>{tokenBalance?.displayValue}</b> {tokenBalance?.symbol}
+              </p>
+            </div>
+          </div>
+
+          <Web3Button
+            action={(contract) => contract.call("claimRewards")}
+            contractAddress={stakingContractAddress}
+          >
+            Claim Rewards
+          </Web3Button>
+
+          <hr className={`${styles.divider} ${styles.spacerTop}`} />
+          <h2>Your Staked NFTs</h2>
+          <div className={styles.nftBoxGrid}>
+            {stakedTokens &&
+              stakedTokens[0]?.map((stakedToken: BigNumber) => (
+                <p key={stakedToken.toString()}>
+                  tokenId={stakedToken.toNumber()}
+                </p>
+              ))}
+          </div>
+
+          <hr className={`${styles.divider} ${styles.spacerTop}`} />
+          <h2>Your Unstaked NFTs</h2>
+          <div className={styles.nftBoxGrid}>
+            {ownedNfts?.map((nft) => (
+              <div className={styles.nftBox} key={nft.metadata.id.toString()}>
+                <ThirdwebNftMedia
+                  metadata={nft.metadata}
+                  className={styles.nftMedia}
+                />
+                <h3>{nft.metadata.name}</h3>
+                <Web3Button
+                  contractAddress={stakingContractAddress}
+                  action={() => stakeNft(nft.metadata.id)}
+                >
+                  Stake
+                </Web3Button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
